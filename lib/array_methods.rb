a270da33b9c643b5d8a765c69df880690609fbaf
Rick Lim <rick.jason.a.lim@gmail.com>
2019-10-28 10:09:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,22 +10,32 @@ using_sort = ["Liverpool", "Man City", "Leicester", "Chelsea"]
 using_sort.sort
 end
 
+p using_sort
+
 def using_reverse
 using_reverse = ["Liverpool", "Man City", "Leicester", "Chelsea"]
 using_reverse.reverse
 end
+
+p using_reverse
 
 def using_first
 using_first = ["Liverpool", "Man City", "Leicester", "Chelsea"]
 using_first.first
 end
 
+p using_first
+
 def using_last
 using_last = ["Liverpool", "Man City", "Leicester", "Chelsea"]
 using_last.last
 end
 
+p using_last
+
 def using_size
 using_size = ["Liverpool", "Man City", "Leicester", "Chelsea"]
 using_size.size
 end
+
+p using_size
